2e83cb5282961a1b381377e4e4a1f87459b202e7
SehyeongLee <shseoul14@gmail.com>
2020-08-29 15:01:05 +0900

    Delete tmp folder

--- monitoring-react-web/src/RealTimeChart.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
-
-const RealTimeChart = props => <Line data={props.data} options={props.options} />;
-
-export default RealTimeChart;
